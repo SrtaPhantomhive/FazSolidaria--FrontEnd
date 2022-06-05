03a52e0780e93e45a5f532f2577958e57bc514f2
gerson espejo <gersonespejo127@gmail.com>
2022-06-04 21:16:36 -0300

    criação do angular provisorio

---
+++ FazSolidaria-Angular/FazSolidaria/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+// import { NgModule} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  
+
+})
+
+
+export class AppRoutingModule { }
+
+
+
