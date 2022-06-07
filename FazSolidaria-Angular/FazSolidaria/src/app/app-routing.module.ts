0887e5401e462cc75de8ed93aec92ed19b6c1177
SrtaPhantomhive <rafaela.ncspp@gmail.com>
2022-06-06 23:09:04 -0300

    Alterações na página inicial, GIFs solucionados. Dar atenção a pág de formulário!

--- FazSolidaria-Angular/FazSolidaria/src/app/app-routing.module.ts
+++ FazSolidaria-Angular/FazSolidaria/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TelaApresentacaoComponent } from './tela-apresentacao/tela-apresentacao.component';
 import { LogarComponent } from './logar/logar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { FormularioVisitaComponent } from './formulario-visita/formulario-visita.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'agendamento', component: FormularioVisitaComponent },
   { path: 'home', component: TelaApresentacaoComponent },
   { path: 'logar', component: LogarComponent },
   { path: 'cadastrar', component: CadastrarComponent }
