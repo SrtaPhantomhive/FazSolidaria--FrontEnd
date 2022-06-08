500fb54b9ccd606cc8b7346ab20fcd00a6db449b
gerson espejo <gersonespejo127@gmail.com>
2022-06-07 23:04:56 -0300

    Scrooll lento adicionado a tela-principa/menu

--- FazSolidaria-Angular/FazSolidaria/src/app/app-routing.module.ts
+++ FazSolidaria-Angular/FazSolidaria/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { TelaApresentacaoComponent } from './tela-apresentacao/tela-apresentacao.component';
 import { LogarComponent } from './logar/logar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
-<<<<<<< HEAD
+
 import { NgModule } from '@angular/core';
 
-=======
+
 import { FormularioVisitaComponent } from './formulario-visita/formulario-visita.component';
->>>>>>> 0887e5401e462cc75de8ed93aec92ed19b6c1177
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
