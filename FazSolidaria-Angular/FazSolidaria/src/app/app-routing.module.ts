ba520559d97c23fdf133a85391bc8aa22c5a2d8d
Juliana Azevedo <juliana_azevedo@hotmail.com.br>
2022-06-08 10:29:23 -0300

    adicionando produtos relacionados

--- FazSolidaria-Angular/FazSolidaria/src/app/app-routing.module.ts
+++ FazSolidaria-Angular/FazSolidaria/src/app/app-routing.module.ts
@@ -2,19 +2,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { TelaApresentacaoComponent } from './tela-apresentacao/tela-apresentacao.component';
 import { LogarComponent } from './logar/logar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import { NgModule } from '@angular/core';
-
-=======
 import { FormularioVisitaComponent } from './formulario-visita/formulario-visita.component';
->>>>>>> 0887e5401e462cc75de8ed93aec92ed19b6c1177
+import { PerfilProdutoComponent } from './perfil-produto/perfil-produto.component';
+// >>>>>>> 0887e5401e462cc75de8ed93aec92ed19b6c1177
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'agendamento', component: FormularioVisitaComponent },
   { path: 'home', component: TelaApresentacaoComponent },
   { path: 'login', component: LogarComponent },
-  { path: 'cadastrar', component: CadastrarComponent }
+  { path: 'cadastrar', component: CadastrarComponent },
+  {path: 'perfil-produtos', component: PerfilProdutoComponent}
 
 ];
 
